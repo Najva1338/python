2ab98a443c6d56f5a4fc190cfaa64c2e840dc702
Najva1338 <Najvamuzammil@gmail.com>
2026-08-02 20:42:44 +0530

    Add files via upload

---
+++ SN.py
@@ -0,0 +1,14 @@
+num1 = float(input("enter a number:"))
+num2 = float(input("enter another number:"))
+
+print("Addition:",num1+num2)
+print("Substraction:",num1-num2)
+print("multiplication:",num1*num2)
+if num2 !=0:
+    print("Division:",num1/num2)
+else:
+    print("division:cannot divide by zero")
+
+
+
+
